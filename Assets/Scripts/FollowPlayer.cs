d12149d7597d0e80ce997edc7b26890a7c742542
Chronoportation <georgiapetroff@gmail.com>
2021-01-29 10:45:21 -0800

    Updated FollowPlayer
    Restricted Camera movement to not go out of bounds

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,16 +6,21 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0f, 15f, 0f);
+    private float minX = -10.2f;
+    private float maxX = 10.2f;
+    private float minZ = -17.3f;
+    private float maxZ = 17.3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //make sure the camera is in the right spot before checking out the bounds
+        transform.position = player.transform.position + offset;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //offset camera behind player and follow it
-        transform.position = player.transform.position + offset;
+        //camera's position will be restricted to not go off the level
+        transform.position = new Vector3(Mathf.Clamp(player.transform.position.x, minX, maxX), transform.position.y, Mathf.Clamp(player.transform.position.z, minZ, maxZ));
     }
 }
